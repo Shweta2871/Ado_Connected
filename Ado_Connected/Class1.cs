c8f482250406557a87345886a5d1b75059166b9c
Shweta2871 <moreshweta309@gmail.com>
2022-08-11 10:20:19 +0530

    Add project files.

---
+++ Ado_Connected/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ado_Connected
+{
+    internal class Class1
+    {
+
+    }
+}
---
+++ Ado_Connected/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+namespace Ado_Connected
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection("Data Source = LAPTOP-20VKH6BS\\SQLEXPRESS; initial catalog = PracticeDB; integrated security = true ");
+                SqlCommand com = new SqlCommand();
+                com.Connection = con;
+
+                com.CommandText = "select avg(salary) from employees";
+                con.Open();
+                decimal Avg = (decimal)com.ExecuteScalar();
+                Console.WriteLine("Average salary of employee = " + Avg);
+                con.Close();
+
+                
+            }
+            catch (SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadKey(); 
+            
+        }
+    }
+}
